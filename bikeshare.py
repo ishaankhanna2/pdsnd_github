33a5b082a6215754cf354dd18c10dc20ff851aa6
Ishaan Khanna <ishaankhanna2@gmail.com>
2022-03-20 18:24:47 -0500

    Updated docstring documentation for stationstats function

--- bikeshare.py
+++ bikeshare.py
@@ -118,7 +118,8 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """Displays statistics on the most popular stations and trip.
+    -- 20220320 This function will output the analysis results ran on the data set defined in the get_filters function above (Ishaan) --"""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
